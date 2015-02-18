f127e70527894cd659a1e85e6cc245d47eefbab5
david viramontes <dviramontes@gmail.com>
2015-02-17 21:56:56 -0700

    init

---
+++ comp.rb
@@ -0,0 +1,92 @@
+# C	 D	E  F	G	A	B
+# 60 62	64 65	67	69	71
+
+@sleep = 0.5
+use_synth :sine
+
+# 1.
+play 60
+sleep 0.5
+play 60
+sleep 0.5
+play 67
+sleep 0.5
+play 67
+sleep 0.5
+play 69
+sleep 0.5
+play 69
+sleep 0.5
+play 67
+sleep 1
+
+# 2.
+play 65
+sleep 0.5
+play 65
+sleep 0.5
+play 64
+sleep 0.5
+play 64
+sleep 0.5
+play 62
+sleep 0.5
+play 62
+sleep 0.5
+play 60
+sleep 1
+
+# 3.
+2.times do 
+	play 67
+	sleep 0.5
+	play 67
+	sleep 0.5
+	play 65
+	sleep 0.5
+	play 65
+	sleep 0.5
+	play 64
+	sleep 0.5
+	play 64
+	sleep 0.5
+	play 62
+	sleep 1
+end
+
+# 4.
+play 60
+sleep 0.5
+play 60
+sleep 0.5
+play 67
+sleep 0.5
+play 67
+sleep 0.5
+play 69
+sleep 0.5
+play 69
+sleep 0.5
+play 67
+sleep 1
+
+# 5.
+play 65
+sleep 0.5
+play 65
+sleep 0.5
+play 64
+sleep 0.5
+play 64
+sleep 0.5
+play 62
+sleep 0.5
+play 62
+sleep 0.5
+play 60
+sleep 1
+
+
+
+
+
